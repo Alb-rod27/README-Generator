189f233acc70817ac08e3fb34c73b891c9b1d9be
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-02-15 10:55:47 -0800

    created util folder and generatePage file

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-//npm install and then npm i  
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 
-//work on inquire function
+// TODO: Create an array of questions for user input
+const questions = [];
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
